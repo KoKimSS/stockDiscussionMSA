8243e913ff90ea468a75e4152f7f57f183981c47
KoKimss <seungsu8848@ajou.ac.kr>
2024-02-14 18:29:55 +0900

    feat : prd url변경

--- stockBatch/src/main/java/com/example/stockbatch/StockBatchApplication.java
+++ stockBatch/src/main/java/com/example/stockbatch/StockBatchApplication.java
@@ -1,11 +1,19 @@
 package com.example.stockbatch;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
+@Slf4j
 public class StockBatchApplication {
+    @Value("${spring.datasource.url}")
+    private static String message;
     public static void main(String[] args) {
+        log.info("url은 {}",message);
+
         SpringApplication.run(StockBatchApplication.class, args);
     }
 }
